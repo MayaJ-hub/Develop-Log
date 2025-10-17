a2dc71d5fc5f67eca9c33cbba677f3e9a80ecf69
MayaJ-hub <mhuggins-jordan@icstars.org>
2025-10-17 18:36:16 -0500

    wellness

---
+++ script.js
@@ -0,0 +1,20 @@
+let username = "Maya";
+let number = "6";
+console.log (number );
+let numbers = 6;
+let x = number + numbers;
+console.log(x)
+let one = 1;
+let z = numbers+ one;
+console.log(z);
+
+let oneword = "Maya";
+let twoword = "Jordan";
+let cybersecurity = oneword + "" + twoword;
+console.log ()
+
+function add(a,b){
+
+    let c = a + b 
+
+}
